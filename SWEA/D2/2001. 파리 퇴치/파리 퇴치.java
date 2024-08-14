b48ddd932b0967f3ed64ebb6d16fe38ca844ab3e
Choe Yunji <ynco32@gmail.com>
2024-08-14 13:14:53 +0900

    [D2] Title: 파리 퇴치, Time: 162 ms, Memory: 21,136 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,41 +1,42 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Solution {
-    public static void main(String[] args) throws FileNotFoundException {
-         
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-         
-        for (int t = 1; t <= tc; t++) {
-            int N = sc.nextInt();
-            int M = sc.nextInt();
-            int[][] data = new int[N][N];
-             
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    data[i][j] = sc.nextInt();
-                }
-            }
-             
-            int sum = 0;
-            for (int k = 0; k <= N-M; k++) {
-                for (int l = 0; l <= N-M; l++) {
-                    int tempSum = 0;
-                    for (int i = k; i < k+M; i++) {
-                        for (int j = l; j < l+M; j++) {
-                            tempSum += data[i][j];
-                        }
-                    }
-                     
-                    sum = Math.max(sum, tempSum);
-                     
-                }
-            }
-            System.out.println("#" + t + " " + sum);
-             
-             
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		
+		int tc = Integer.parseInt(st.nextToken());
+		
+		for (int t= 1; t <= tc; t++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			int[][] mapSum = new int[N+1][N+1];
+			
+			int sum = Integer.MIN_VALUE;
+			for (int i = 1; i < N + 1; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 1; j < N + 1; j++) {
+					int temp = Integer.parseInt(st.nextToken());
+					mapSum[i][j] = temp + mapSum[i - 1][j] + mapSum[i][j - 1] - mapSum[i - 1][j - 1];
+				}
+			}
+
+			for (int i = M; i < N + 1; i++) {
+				for (int j = M; j < N + 1; j++) {
+					int tempSum = mapSum[i][j] - mapSum[i][j - M] - mapSum[i - M][j] + mapSum[i - M][j - M];
+					if (tempSum > sum)
+						sum = tempSum;
+				}
+			}
+			
+			System.out.println("#"+t + " " + sum);
+			
+		}
+		
+	}
 }
