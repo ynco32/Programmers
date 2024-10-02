d493cae73b6611f8a71c1563e47e67344129974c
Choe Yunji <ynco32@gmail.com>
2024-10-02 11:35:17 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 128 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,54 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/*
+ * 
+ 6
+10 20 10 30 20 50
+ */
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] best = new int[N];
+		int[] prev = new int[N];
+		int[] num = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+			prev[i] = i;
+			best[i] = 1;
+		}
+
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				if (num[i] > num[j] && best[i] < best[j] + 1) {
+					best[i] = best[j] + 1;
+					prev[i] = j;
+				}
+			}
+		}
+
+		int ans = 0;
+		int idx = 0;
+		for (int i = 0; i < N; i++) {
+			if (ans < best[i]) {
+				ans = Math.max(ans, best[i]);
+				idx = i;
+			}
+		}
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < ans; i++) {
+			list.add(num[idx]);
+			idx = prev[idx];
+		}
+		System.out.println(ans);
+		for (int i = 1; i <= ans; i++) {
+			System.out.print(list.get(ans - i) + " ");
+		}
+	}
+}
