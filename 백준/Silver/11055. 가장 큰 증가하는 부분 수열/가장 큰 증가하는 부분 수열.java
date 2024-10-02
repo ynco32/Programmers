609634149c0b9126b3835d9a834b0a3b806e27b5
Choe Yunji <ynco32@gmail.com>
2024-10-02 10:28:01 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 120 ms, Memory: 14528 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,42 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 
+ 6
+10 20 10 30 20 50
+ */
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] best = new int[N];
+		int[] prev = new int[N];
+		int[] num = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+			prev[i] = i;
+			best[i] = num[i];
+		}
+
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				if (num[i] > num[j] && best[i] < best[j] + num[i]) {
+					best[i] = best[j] + num[i];
+					prev[i] = j;
+				}
+			}
+		}
+
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			ans = Math.max(ans, best[i]);
+		}
+
+		System.out.println(ans);
+	}
+}
