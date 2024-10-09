45ec54afb209778e4ed6386fffda18a9b0322f49
Choe Yunji <ynco32@gmail.com>
2024-10-09 15:31:51 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,8 @@
--- 코드를 입력하세요
-SELECT NAME, DATETIME
-FROM ANIMAL_INS
-WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID
-                        FROM ANIMAL_OUTS)
-ORDER BY DATETIME
-LIMIT 3
+select NAME, DATETIME
+from ANIMAL_INS
+where ANIMAL_ID not in (
+    select ANIMAL_ID
+    from ANIMAL_OUTS
+    )
+order by DATETIME
+limit 3
