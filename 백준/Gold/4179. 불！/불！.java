d80bb5c1be258ba285df0f9cd9eb414f3e27207a
Choe Yunji <ynco32@gmail.com>
2024-12-13 17:26:36 +0900

    [Gold III] Title: 불!, Time: 512 ms, Memory: 81128 KB -BaekjoonHub

---
+++ 백준/Gold/4179. 불！/불！.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] dr = {1, -1, 0, 0};
+    static int[] dc = {0, 0, 1, -1};
+    static int N, M;
+    static char[][] map;
+    static int[][] fireMap;
+    static int[][] jMap;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new char[N][M];
+        fireMap = new int[N][M];
+        jMap = new int[N][M];
+
+        Queue<int[]> fireQueue = new LinkedList<>();
+        Queue<int[]> jQueue = new LinkedList<>();
+
+        // Initialize maps
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j);
+                fireMap[i][j] = Integer.MAX_VALUE;
+                jMap[i][j] = Integer.MAX_VALUE;
+
+                if (map[i][j] == 'F') {
+                    fireQueue.offer(new int[]{i, j});
+                    fireMap[i][j] = 0;
+                } else if (map[i][j] == 'J') {
+                    jQueue.offer(new int[]{i, j});
+                    jMap[i][j] = 0;
+                }
+            }
+        }
+
+        // Spread fire BFS
+        while (!fireQueue.isEmpty()) {
+            int[] current = fireQueue.poll();
+            for (int d = 0; d < 4; d++) {
+                int nr = current[0] + dr[d];
+                int nc = current[1] + dc[d];
+
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                if (map[nr][nc] == '#') continue;
+                if (fireMap[nr][nc] <= fireMap[current[0]][current[1]] + 1) continue;
+
+                fireMap[nr][nc] = fireMap[current[0]][current[1]] + 1;
+                fireQueue.offer(new int[]{nr, nc});
+            }
+        }
+
+        // Character escape BFS
+        while (!jQueue.isEmpty()) {
+            int[] current = jQueue.poll();
+
+            // Check if we can escape at this point
+            if (current[0] == 0 || current[0] == N - 1 || current[1] == 0 || current[1] == M - 1) {
+                System.out.println(jMap[current[0]][current[1]] + 1);
+                return;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nr = current[0] + dr[d];
+                int nc = current[1] + dc[d];
+
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                if (map[nr][nc] == '#') continue;
+                if (jMap[nr][nc] <= jMap[current[0]][current[1]] + 1) continue;
+                if (fireMap[nr][nc] <= jMap[current[0]][current[1]] + 1) continue;
+
+                jMap[nr][nc] = jMap[current[0]][current[1]] + 1;
+                jQueue.offer(new int[]{nr, nc});
+            }
+        }
+
+        System.out.println("IMPOSSIBLE");
+    }
+}
