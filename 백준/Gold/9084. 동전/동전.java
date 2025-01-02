5c61ec860ea6b33e11b8b11020da2a0ae27852a8
Choe Yunji <ynco32@gmail.com>
2025-01-02 13:40:35 +0900

    [Gold V] Title: 동전, Time: 124 ms, Memory: 14840 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < tc; t++) {
+            int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] coins = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                coins[i] = Integer.parseInt(st.nextToken());
+            }
+            int m = Integer.parseInt(br.readLine());
+            int[] dp = new int[m + 1];
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= m; j++) {
+                    if (j - coins[i] > 0) {
+                        dp[j] = dp[j] + dp[j - coins[i]];
+                    } else if (j - coins[i] == 0) {
+                        dp[j]++;
+                    }
+                }
+            }
+            sb.append(dp[m] + "\n");
+        }
+        System.out.print(sb);
+    }
+}
